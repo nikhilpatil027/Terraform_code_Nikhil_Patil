55519fed63a77e462bab671b8736388c2fdb0903
nikhilpatil027 <nikhilrpatil027@gmail.com>
2025-03-06 21:26:09 +0530

    Add files via upload

---
+++ Modules/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 3.0"
+    }
+  }
+}
+provider "aws" {
+    region = var.region
+  
+}
+module "ec2_instance" {
+  source        = "github.com/nikhilpatil027/Terraform_code_Nikhil_Patil/Modules/ec2_creation"
+  region        = var.region
+  instance_type = var.instance_type
+}
